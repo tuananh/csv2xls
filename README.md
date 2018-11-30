63e53d7f9565f73b1e87925536434197fd6576a2
Tuan Anh Tran <me@tuananh.org>
2018-11-30 22:24:41 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+csv2xls
+=======
+
+> I couldn't find a way to import CSV file in Excel properly when CSV file value contains line break 😱
+
+## Usage
+
+    node --max-old-space-size=4096 index.js mycsvfile.csv
---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const csv = require('csv-parser')
+const xlsx = require('node-xlsx')
+
+// Excel sucks when dealing with CSV value contains line breaks
+
+const results = []
+async function makeExcelHappy() {
+    const fileName = process.argv[2]
+    if (fileName && fileName.indexOf('.csv' > -1 )) {
+        fs.createReadStream(fileName)
+            .pipe(csv())
+            .on('data', (data) => {
+                results.push(Object.keys(data).map(key => data[key]))
+            })
+            .on('end', () => {
+                console.log('start writing xls file')
+                const buffer = xlsx.build([{name: 'csv2xls', data: results}])
+                const fileNameWithoutExt = fileName.replace('.csv', '')
+
+                fs.writeFile(fileNameWithoutExt + '.xlsx', buffer, function (err) {
+                    if (err) {
+                        console.log('error while exporting to xlsx', err)
+                    } else {
+                        console.log('file exported successfully')
+                    }
+                })
+            })
+    } else {
+        console.log('expect a csv file name as input')
+    }
+}
+
+makeExcelHappy()
